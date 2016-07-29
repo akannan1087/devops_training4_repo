546e9a957660530432bebd0fe23db957e6180ce2
akannan <akannan@corelogic.com>
2016-07-28 23:59:14 -0500

    new folder

---
+++ MySeleniumWebTest/src/test/java/com/org/selenium/AppTest.java
@@ -0,0 +1,44 @@
+package com.org.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+        
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.indeed.co.uk");
+    }
+}
